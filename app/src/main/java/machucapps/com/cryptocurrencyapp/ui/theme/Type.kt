fb9960e4ac516f4169d9f7aee8b14ae978bb4d7b
Fernando Sanz <machucapps@gmail.com>
2021-10-17 11:27:43 +0200

    Project resources have been added.

--- app/src/main/java/machucapps/com/cryptocurrencyapp/ui/theme/Type.kt
+++ app/src/main/java/machucapps/com/cryptocurrencyapp/ui/theme/Type.kt
@@ -8,21 +8,29 @@ import androidx.compose.ui.unit.sp
 
 // Set of Material typography styles to start with
 val Typography = Typography(
-        body1 = TextStyle(
-                fontFamily = FontFamily.Default,
-                fontWeight = FontWeight.Normal,
-                fontSize = 16.sp
-        )
-        /* Other default text styles to override
-    button = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.W500,
-        fontSize = 14.sp
+    h1 = TextStyle(
+        fontWeight = FontWeight.Bold,
+        fontSize = 30.sp
     ),
-    caption = TextStyle(
+    h2 = TextStyle(
+        fontWeight = FontWeight.SemiBold,
+        fontSize = 24.sp
+    ),
+    h3 = TextStyle(
+        fontWeight = FontWeight.Medium,
+        fontSize = 20.sp
+    ),
+    h4 = TextStyle(
+        fontWeight = FontWeight.Medium,
+        fontSize = 16.sp
+    ),
+    body1 = TextStyle(
         fontFamily = FontFamily.Default,
         fontWeight = FontWeight.Normal,
-        fontSize = 12.sp
+        fontSize = 16.sp
+    ),
+    body2 = TextStyle(
+        fontWeight = FontWeight.Normal,
+        fontSize = 14.sp
     )
-    */
 )
